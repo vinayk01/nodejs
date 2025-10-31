c9fd56a6168838347a1865c2768877e236f93953
root <root@DS-CP-LAP-003>
2025-10-31 13:41:43 +0530

    port changed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
