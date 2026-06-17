08cbc8dc10ddad5a4a517c364be683f9506c8068
root <root@ip-10-0-1-127.ec2.internal>
2026-06-17 11:07:14 +0000

    b

--- app.js
+++ app.js
@@ -93,4 +93,8 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
-
+app.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
